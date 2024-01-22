b9b9c26c328d0724697c072b7cca288ef05b890b
sreedhar-98 <sreedhardronamraju1@gmail.com>
2024-01-23 00:58:57 +0530

    login

--- netflix-gpt/src/components/Login.js
+++ netflix-gpt/src/components/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
       <img
         src="https://assets.nflxext.com/ffe/siteui/vlv3/16006346-87f9-4226-bc25-a1fb346a2b0c/9662d0fd-0547-4665-b887-771617268815/IN-en-20240115-popsignuptwoweeks-perspective_alpha_website_medium.jpg"
         alt="bgimage"
-        className="absolute w-full h-full md:block hidden opacity-80"
+        className="absolute w-full h-full object-cover md:block hidden opacity-80"
       ></img>
       <SIgnInSignUp />
     </div>
