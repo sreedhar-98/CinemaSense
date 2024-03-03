3a335e1053c52ed621f6e17590baa44e0294af28
sreedhar-98 <sreedhardronamraju1@gmail.com>
2024-03-03 21:31:58 +0530

    Handled duplicate movies

--- netflix-gpt/src/components/MovieCard.js
+++ netflix-gpt/src/components/MovieCard.js
@@ -4,7 +4,10 @@ import ChevronUp from "../utils/Svg/ChevronUp.svg";
 import addIcon from "../utils/Svg/addIcon.svg";
 import Genres from "../utils/Genres.json";
 import { posterURL } from "../utils/urls";
-import { useAddMovieMutation, useGetMoviesDataQuery } from "../utils/list_api";
+import {
+  useAddMovieMutation,
+  useLazyGetMoviesDataQuery,
+} from "../utils/list_api";
 import { useSelector } from "react-redux";
 
 const MovieCard = memo(
@@ -20,6 +23,7 @@ const MovieCard = memo(
       backdrop_path,
     } = movie;
     const user_data = useSelector((store) => store.user.data);
+    const [isExists, setIsExists] = useState(false);
     const uid = user_data?.uid;
     const genres_list = Genres.genres;
     const genres = genre_ids.map((id) => {
@@ -39,17 +43,23 @@ const MovieCard = memo(
     };
     const url = posterURL + poster_path;
     const [addMovie, status_data] = useAddMovieMutation();
-    const {isFetching,isError,data}=useGetMoviesDataQuery(
-      {
-        uid: user_data.data?.uid,
-      },
-      { skip: user_data.data?.uid ? false : true }
-    );
 
-    console.log(data);
+    const [trigger,{isUninitialized}] = useLazyGetMoviesDataQuery();
 
-    const addMovieHandler = () => {
-      addMovie({ uid: uid, movie: db_movie });
+    console.log("Movie Called",movie?.id);
+
+    const addMovieHandler = async () => {
+      const { data, isSuccess } = await trigger({ uid: uid }, true);
+      if (isSuccess) {
+        const isExists = data?.movies.some(
+          (m) => m?.id === movie?.id.toString()
+        );
+        if (!isExists) {
+          addMovie({ uid: uid, movie: db_movie });
+          setIsExists(false);
+        }
+        else setIsExists(true);
+      }
     };
 
     return (
@@ -57,19 +67,25 @@ const MovieCard = memo(
         <div className="relative group/card bg-black flex flex-col gap-1 hover:scale-110 duration-200 rounded-md  hover:z-50 overflow-hidden flex-shrink-0">
           <img src={url} alt="Img" className="object-cover w-full"></img>
 
-          <div
-            className={`absolute w-full ${
-              status_data?.isSuccess ? "bg-green-600" : "bg-red-600"
-            } text-white text-base mt-3 ${
-              status_data.isUninitialized || status_data?.isLoading
-                ? "hidden"
-                : "visible"
-            }`}
-          >
-            <span className="px-1">
-              {status_data?.isSuccess ? "Added to List" : "Error! Try Again"}
-            </span>
-          </div>
+          {!isExists ? (
+            <div
+              className={`absolute w-full ${
+                status_data?.isSuccess ? "bg-green-600" : "bg-red-600"
+              } text-white text-base mt-3 ${
+                (status_data.isUninitialized || status_data?.isLoading)
+                  ? "hidden"
+                  : "visible"
+              }`}
+            >
+              <span className="px-1">
+                {status_data?.isSuccess ? "Added to List" : "Error! Try Again"}
+              </span>
+            </div>
+          ) : (
+            <div className={`absolute w-full bg-orange-600 text-white text-base mt-3 ${isUninitialized?"hidden":"visible"}`}>
+              <span className="px-1 text-wrap">Movie Already in List</span>
+            </div>
+          )}
           <div className="relative hidden group-hover/card:block bg-slate-800 w-full">
             <div className="flex flex-wrap gap-2 items-center">
               <div className="flex gap-1 mx-2 my-2 flex-wrap mb-2">
